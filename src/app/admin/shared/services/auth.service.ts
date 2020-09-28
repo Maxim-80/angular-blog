434f940d960e7857cb5ad901da973ccd4bbb679d
Maxim_80 <18kmv@mail.ru>
2020-09-28 14:53:59 +0300

    добавил обработку ошибок при авторизации

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {FbAuthResponse, User} from "../interfaces";
-import {from, Observable} from "rxjs";
+import {from, Observable, Subject, throwError} from 'rxjs';
 import { environment } from 'src/environments/environment';
-import {tap} from 'rxjs/operators'
+import {catchError, tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
+
+  public error$: Subject<string> = new Subject<string>()
 
   constructor(private  http: HttpClient) { }
 
@@ -23,7 +25,8 @@ export class AuthService {
     user.returnSecureToken = true
     return  this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
     .pipe(
-      tap(this.setToken)
+      tap(this.setToken),
+      catchError(this.handleError.bind(this))
     )
   }
 
@@ -43,5 +46,23 @@ export class AuthService {
     } else {
       localStorage.clear()
     }
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const {message} = error.error.error
+
+    switch (message) {
+      case 'INVALID_EMAIL':
+        this.error$.next('Неверный имэйл')
+        break
+      case 'EMAIL_NOT_FOUND':
+        this.error$.next('Имейл не найден')
+        break
+      case 'INVALID_PASSWORD':
+        this.error$.next('Неверный пароль')
+        break
+    }
+
+    return throwError(error)
   }
 }
