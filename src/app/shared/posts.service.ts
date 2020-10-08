8cff219fbfaaec76a42db1178b82059e05f06fad
Maxim_80 <18kmv@mail.ru>
2020-10-08 15:30:48 +0300

    разметил страницу редактирования поста

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -37,4 +37,15 @@ export class PostsService {
   remove(id: string): Observable<void> {
     return  this.http.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`)
   }
+
+  getById(id: string): Observable<Post> {
+    return this.http.get(`${environment.fbDbUrl}/posts/${id}.json`)
+      .pipe(map((post: Post) => {
+        return {
+          ...post,
+          id,
+          date: new Date(post.date)
+        }
+      }))
+  }
 }
