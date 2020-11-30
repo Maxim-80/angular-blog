fd07465000edd1eb5e0d983aac06966e6b75baec
zadnica <zadnica768@gmail.com>
2020-11-30 22:01:42 +0300

    Добавил обновление приложения у клиента

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(private updates: SwUpdate) {
+    this.updates.available.subscribe(event => {
+      if (confirm('Доступно обновление. Обновить сейчас?')) {
+        this.updates.activateUpdate().then(() => document.location.reload())
+      }
+    })
+  }
 }
